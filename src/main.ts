11120f4e62642c80163b518fd5d5494ee3db2911
J-O Racine <joracine@gmail.com>
2020-07-15 22:15:44 -0700

    Initial Project Setup
    Setup project with Typescript, Mocha, Chai, ts-node and a sample test.

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { sayHello } from './Calcualtor';
+
+console.log(sayHello('TypeScript'));
+
+function hello(compiler: string) {
+    console.log(`Hello from ${compiler}`);
+}
+
+hello('TypeScript');
